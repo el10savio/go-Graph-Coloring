722cf6595a7c42a74c591603cb275de2724a1a14
Elton SV <el10savio96@gmail.com>
2019-12-01 20:01:52 +0530

    Added Tests & Refactored

--- graphcoloring/graphcoloring.go
+++ graphcoloring/graphcoloring.go
@@ -12,18 +12,18 @@ type Node struct {
 	Color int
 }
 
-func (graph *Graph) addNode(value int, links []int) {
-	node := Node{value, links, -1}
-	graph.AdjacencyList = append(graph.AdjacencyList, node)
-}
-
-func (graph *Graph) display() {
+func (graph *Graph) Display() {
 	for _, node := range graph.AdjacencyList {
 		fmt.Println(node)
 	}
 }
 
-func (graph *Graph) findNode(value int) Node {
+func (graph *Graph) AddNode(value int, links []int) {
+	node := Node{value, links, -1}
+	graph.AdjacencyList = append(graph.AdjacencyList, node)
+}
+
+func (graph *Graph) FindNode(value int) Node {
 	var nodeFound Node
 
 	for _, node := range graph.AdjacencyList {
@@ -35,35 +35,43 @@ func (graph *Graph) findNode(value int) Node {
 	return nodeFound
 }
 
-func (graph *Graph) findMinNode() Node {
+func (graph *Graph) FindMinNode() Node {
 	var minNode Node
 
-	for _, node := range graph.AdjacencyList {
-		if node.Value < minNode.Value {
-			minNode = node
+	if len(graph.AdjacencyList) > 0 {
+		minNode = graph.AdjacencyList[0]
+
+		for index := 1; index < len(graph.AdjacencyList); index++ {
+			node := graph.AdjacencyList[index]
+			if node.Value < minNode.Value {
+				minNode = node
+			}
 		}
 	}
 
 	return minNode
 }
 
-func (graph *Graph) generateColors() {
+func (graph *Graph) GenerateColors() {
 	verticesCount := len(graph.AdjacencyList)
-	graph.AdjacencyList[0].Color = 0
+	if verticesCount > 0 {
+		graph.AdjacencyList[0].Color = 0
+		availableColorList := InitColorMap(verticesCount)
 
-	for index := 1; index < verticesCount; index++ {
-		availableColors := initColorMap(verticesCount)
-		for _, link := range graph.AdjacencyList[index].Links {
-			linkNode := graph.findNode(link)
-			if linkNode.Color != -1 {
-				availableColors[linkNode.Color] = false
+		for index := 1; index < verticesCount; index++ {
+			availableColors := availableColorList
+			for _, link := range graph.AdjacencyList[index].Links {
+				linkNode := graph.FindNode(link)
+				if linkNode.Color != -1 {
+					availableColors[linkNode.Color] = false
+				}
 			}
+			graph.AdjacencyList[index].Color = GetFirstAvailableColor(availableColors)
 		}
-		graph.AdjacencyList[index].Color = getFirstAvailableColor(availableColors)
 	}
 }
 
-func initColorMap(count int) map[int]bool {
+func InitColorMap(count int) map[int]bool {
 	colorMap := make(map[int]bool)
 
 	for index := 0; index < count; index++ {
@@ -73,7 +81,7 @@ func initColorMap(count int) map[int]bool {
 	return colorMap
 }
 
-func getFirstAvailableColor(unavailableColors map[int]bool) int {
+func GetFirstAvailableColor(unavailableColors map[int]bool) int {
 	var availableColor int
 
 	for index := 0; index < len(unavailableColors); index++ {
---
+++ graphcoloring/graphcoloring_test.go
@@ -0,0 +1,280 @@
+package graphcoloring
+
+import (
+	"reflect"
+	"testing"
+)
+
+var graphTest = Graph{
+	AdjacencyList: []Node{
+		Node{Value: 1, Links: []int{2, 3}, Color: -1},
+		Node{Value: 2, Links: []int{1, 3}, Color: -1},
+		Node{Value: 3, Links: []int{1, 2}, Color: -1},
+	},
+}
+
+func TestGetFirstAvailableColor(t *testing.T) {
+	unavailableColors := map[int]bool{0: false, 1: false, 2: true, 3: false}
+
+	expectedColor := 2
+
+	actualColor := GetFirstAvailableColor(unavailableColors)
+
+	if !reflect.DeepEqual(expectedColor, actualColor) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColor, actualColor)
+	}
+}
+
+func TestGetFirstAvailableColor_NoColors(t *testing.T) {
+	unavailableColors := map[int]bool{}
+
+	expectedColor := 0
+
+	actualColor := GetFirstAvailableColor(unavailableColors)
+
+	if !reflect.DeepEqual(expectedColor, actualColor) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColor, actualColor)
+	}
+}
+
+func TestGetFirstAvailableColor_NoAvailableColors(t *testing.T) {
+	unavailableColors := map[int]bool{0: false, 1: false, 2: false, 3: false}
+
+	expectedColor := 0
+
+	actualColor := GetFirstAvailableColor(unavailableColors)
+
+	if !reflect.DeepEqual(expectedColor, actualColor) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColor, actualColor)
+	}
+}
+
+func TestInitColorMap(t *testing.T) {
+	count := 4
+
+	expectedColorMap := map[int]bool{0: true, 1: true, 2: true, 3: true}
+
+	actualColorMap := InitColorMap(count)
+
+	if !reflect.DeepEqual(expectedColorMap, actualColorMap) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColorMap, actualColorMap)
+	}
+}
+
+func TestInitColorMap_NoCount(t *testing.T) {
+	count := 0
+
+	expectedColorMap := map[int]bool{}
+
+	actualColorMap := InitColorMap(count)
+
+	if !reflect.DeepEqual(expectedColorMap, actualColorMap) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColorMap, actualColorMap)
+	}
+}
+
+func TestInitColorMap_InvalidCount(t *testing.T) {
+	count := -1
+
+	expectedColorMap := map[int]bool{}
+
+	actualColorMap := InitColorMap(count)
+
+	if !reflect.DeepEqual(expectedColorMap, actualColorMap) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedColorMap, actualColorMap)
+	}
+}
+
+func TestFindMinNode(t *testing.T) {
+	graph := graphTest
+
+	expectedNode := Node{Value: 1, Links: []int{2, 3}, Color: -1}
+
+	actualNode := graph.FindMinNode()
+
+	if !reflect.DeepEqual(expectedNode, actualNode) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedNode, actualNode)
+	}
+}
+
+func TestFindMinNode_NoGraph(t *testing.T) {
+	var graph Graph
+
+	expectedNode := Node{}
+
+	actualNode := graph.FindMinNode()
+
+	if !reflect.DeepEqual(expectedNode, actualNode) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedNode, actualNode)
+	}
+}
+
+func TestFindNode(t *testing.T) {
+	graph := graphTest
+	value := 2
+
+	expectedNode := Node{Value: 2, Links: []int{1, 3}, Color: -1}
+
+	actualNode := graph.FindNode(value)
+
+	if !reflect.DeepEqual(expectedNode, actualNode) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedNode, actualNode)
+	}
+}
+
+func TestFindNode_NoGraph(t *testing.T) {
+	var graph Graph
+	value := 2
+
+	expectedNode := Node{}
+
+	actualNode := graph.FindNode(value)
+
+	if !reflect.DeepEqual(expectedNode, actualNode) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedNode, actualNode)
+	}
+}
+
+func TestFindNode_NoValue(t *testing.T) {
+	graph := graphTest
+	value := 0
+
+	expectedNode := Node{}
+
+	actualNode := graph.FindNode(value)
+
+	if !reflect.DeepEqual(expectedNode, actualNode) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedNode, actualNode)
+	}
+}
+
+func TestAddNode(t *testing.T) {
+	graph := graphTest
+	value := 4
+	links := []int{}
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{2, 3}, Color: -1},
+			Node{Value: 2, Links: []int{1, 3}, Color: -1},
+			Node{Value: 3, Links: []int{1, 2}, Color: -1},
+			Node{Value: 4, Links: []int{}, Color: -1},
+		},
+	}
+
+	graph.AddNode(value, links)
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestAddNode_EmptyGraph(t *testing.T) {
+	var graph Graph
+	value := 1
+	links := []int{}
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{}, Color: -1},
+		},
+	}
+
+	graph.AddNode(value, links)
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestGenerateColors(t *testing.T) {
+	graph := graphTest
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{2, 3}, Color: 0},
+			Node{Value: 2, Links: []int{1, 3}, Color: 1},
+			Node{Value: 3, Links: []int{1, 2}, Color: 2},
+		},
+	}
+
+	graph.GenerateColors()
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestGenerateColors_NoGraph(t *testing.T) {
+	var graph Graph
+
+	var expectedGraph Graph
+
+	graph.GenerateColors()
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestGenerateColors_SingleNode(t *testing.T) {
+	var graph Graph
+	graph.AddNode(1, []int{})
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{}, Color: 0},
+		},
+	}
+
+	graph.GenerateColors()
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestGenerateColors_TwoSeperatedNodes(t *testing.T) {
+	var graph Graph
+	graph.AddNode(1, []int{})
+	graph.AddNode(2, []int{})
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{}, Color: 0},
+			Node{Value: 2, Links: []int{}, Color: 0},
+		},
+	}
+
+	graph.GenerateColors()
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
+
+func TestGenerateColors_TwoConnectedNodes(t *testing.T) {
+	var graph Graph
+	graph.AddNode(1, []int{2})
+	graph.AddNode(2, []int{1})
+
+	expectedGraph := Graph{
+		AdjacencyList: []Node{
+			Node{Value: 1, Links: []int{2}, Color: 0},
+			Node{Value: 2, Links: []int{1}, Color: 1},
+		},
+	}
+
+	graph.GenerateColors()
+	actualGraph := graph
+
+	if !reflect.DeepEqual(expectedGraph, actualGraph) {
+		t.Fatalf("\nExpected %v \nGot %v", expectedGraph, actualGraph)
+	}
+}
